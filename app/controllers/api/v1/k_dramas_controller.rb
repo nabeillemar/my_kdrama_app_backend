c4a3f489f359ba32b8633a1da8d307f315582bce
Nabeil <nabeillemar@gmail.com>
2022-02-13 14:28:13 -0800

    updated created action with serializer

--- app/controllers/api/v1/k_dramas_controller.rb
+++ app/controllers/api/v1/k_dramas_controller.rb
@@ -10,7 +10,8 @@ class Api::V1::KDramasController < ApplicationController
     def create #creating new kdramas based on whatever kdrama_params we get from our frontend
        kdrama = KDrama.new(kdrama_params)
        if kdrama.save
-        render json: kdrama, status: :accepted # if the instances is saved then render the data json if it saved sucessfully, status allows us to communicate with the client side 
+         render json: KDramaSerializer.new(kdrama), status: :accepted
+        #render json: kdrama, status: :accepted # if the instances is saved then render the data json if it saved sucessfully, status allows us to communicate with the client side 
        else
         render json: {errors: kdrama.errors.full_messages}, status: :unprocessible_entity #if the instance doesn't save we can send this to the front end to the client (user side) we can display the full messages , need to see what these status come from and what they mean
        end 
