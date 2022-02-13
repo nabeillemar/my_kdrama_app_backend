e21819a46fdef8c47afb938d84e05050a9b3ca01
Nabeil <nabeillemar@gmail.com>
2022-02-13 11:01:41 -0800

    updated kdrama_params and create action

--- app/controllers/api/v1/k_dramas_controller.rb
+++ app/controllers/api/v1/k_dramas_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::KDramasController < ApplicationController
     def create #creating new kdramas based on whatever kdrama_params we get from our frontend
        kdrama = KDrama.new(kdrama_params)
        if kdrama.save
-        render json: syllabus, status: :accepted # if the instances is saved then render the data json if it saved sucessfully, status allows us to communicate with the client side 
+        render json: kdrama, status: :accepted # if the instances is saved then render the data json if it saved sucessfully, status allows us to communicate with the client side 
        else
         render json: {errors: kdrama.errors.full_messages}, status: :unprocessible_entity #if the instance doesn't save we can send this to the front end to the client (user side) we can display the full messages , need to see what these status come from and what they mean
        end 
@@ -20,8 +20,12 @@ class Api::V1::KDramasController < ApplicationController
 
     private
 
+    #def kdrama_params # permit a parameter ... must be includes in the body of the POST or POST requests we will be making with JS fetch 
+     #   params.require(:k_dramas).permit(:title, :release_year, :watched, :where_to_watch, :cover_photo, :my_rating, :comment, :category_id)
+   # end 
+
     def kdrama_params # permit a parameter ... must be includes in the body of the POST or POST requests we will be making with JS fetch 
-        params.require(:k_dramas).permit(:title, :release_year, :watched, :where_to_watch, :cover_photo, :my_rating, :comment, :category_id)
+        params.require(:k_drama).permit(:title, :release_year, :watched, :where_to_watch, :cover_photo, :my_rating, :comment, :category_id)
     end 
 
 end
