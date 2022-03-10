5dde57fa454b32bed6ff48f7eaeeef7a5c5d4be3
Nabeil <nabeillemar@gmail.com>
2022-03-10 08:42:07 -0800

    update

--- app/controllers/api/v1/k_dramas_controller.rb
+++ app/controllers/api/v1/k_dramas_controller.rb
@@ -13,7 +13,7 @@ class Api::V1::KDramasController < ApplicationController
          render json: KDramaSerializer.new(kdrama), status: :accepted
         #render json: kdrama, status: :accepted # if the instances is saved then render the data json if it saved sucessfully, status allows us to communicate with the client side 
        else
-        render json: {errors: kdrama.errors.full_messages}, status: :unprocessible_entity #if the instance doesn't save we can send this to the front end to the client (user side) we can display the full messages , need to see what these status come from and what they mean
+        render json: {errors: kdrama.errors.full_messages} #if the instance doesn't save we can send this to the front end to the client (user side) we can display the full messages , need to see what these status come from and what they mean
        end 
     end 
 
--- app/models/k_drama.rb
+++ app/models/k_drama.rb
@@ -2,4 +2,6 @@ class KDrama < ApplicationRecord
     belongs_to :category
 
     validates :title, presence: true
+    #validates :my_rating, numericality: { only_integer: true }
+
 end
