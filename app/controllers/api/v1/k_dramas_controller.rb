489326bb26d1426fc66a80e3683d3167cb29be2b
Nabeil <nabeillemar@gmail.com>
2022-03-15 14:12:52 -0700

    final update

--- app/controllers/api/v1/k_dramas_controller.rb
+++ app/controllers/api/v1/k_dramas_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::KDramasController < ApplicationController
 
 
     def index #rendering all data of the kdramas in the form of JSON 
-        kdramas = KDrama.all # we don't need to use instance varaibles anymore because of .....
-        #render json: kdramas #this shows you everything #array then the objects
+        kdramas = KDrama.all 
         render json: KDramaSerializer.new(kdramas) #this adds the type, you have to add the attributes that you want to bring in #getting a data object that has an array, we have an attributes object that has an array, #we can restrict the data
     end
 
@@ -11,7 +10,6 @@ class Api::V1::KDramasController < ApplicationController
        kdrama = KDrama.new(kdrama_params)
        if kdrama.save
          render json: KDramaSerializer.new(kdrama), status: :accepted
-        #render json: kdrama, status: :accepted # if the instances is saved then render the data json if it saved sucessfully, status allows us to communicate with the client side 
        else
         render json: {errors: kdrama.errors.full_messages} #if the instance doesn't save we can send this to the front end to the client (user side) we can display the full messages , need to see what these status come from and what they mean
        end 
@@ -46,9 +44,6 @@ class Api::V1::KDramasController < ApplicationController
 
     private
 
-    #def kdrama_params # permit a parameter ... must be includes in the body of the POST or POST requests we will be making with JS fetch 
-     #   params.require(:k_dramas).permit(:title, :release_year, :watched, :where_to_watch, :cover_photo, :my_rating, :comment, :category_id)
-   # end 
 
     def kdrama_params # permit a parameter ... must be includes in the body of the POST or POST requests we will be making with JS fetch 
         params.require(:k_drama).permit(:title, :release_year, :watched, :where_to_watch, :cover_photo, :my_rating, :comment, :category_id)
