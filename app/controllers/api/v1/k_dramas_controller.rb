48dce95c43d70bd9febec8e79634dc55ff7ec414
Nabeil <nabeillemar@gmail.com>
2022-05-22 11:57:24 -0700

    version 1completed  comments removed

--- app/controllers/api/v1/k_dramas_controller.rb
+++ app/controllers/api/v1/k_dramas_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::KDramasController < ApplicationController
 
 
-    def index #rendering all data of the kdramas in the form of JSON 
+    def index 
         kdramas = KDrama.all 
-        render json: KDramaSerializer.new(kdramas) #this adds the type, you have to add the attributes that you want to bring in #getting a data object that has an array, we have an attributes object that has an array, #we can restrict the data
+        render json: KDramaSerializer.new(kdramas) 
     end
 
-    def create #creating new kdramas based on whatever kdrama_params we get from our frontend
+    def create 
        kdrama = KDrama.new(kdrama_params)
        if kdrama.save
          render json: KDramaSerializer.new(kdrama), status: :accepted
        else
-        render json: {errors: kdrama.errors.full_messages} #if the instance doesn't save we can send this to the front end to the client (user side) we can display the full messages , need to see what these status come from and what they mean
+        render json: {errors: kdrama.errors.full_messages} 
        end 
     end 
 
@@ -45,7 +45,7 @@ class Api::V1::KDramasController < ApplicationController
     private
 
 
-    def kdrama_params # permit a parameter ... must be includes in the body of the POST or POST requests we will be making with JS fetch 
+    def kdrama_params 
         params.require(:k_drama).permit(:title, :release_year, :watched, :where_to_watch, :cover_photo, :my_rating, :comment, :category_id)
     end 
 
